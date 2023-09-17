809fc48504b6ac48f781ca891b69011d4d7edc42
gunman_PC_HOME_DESKTOP <gunman97@gmail.com>
2023-09-17 19:26:04 +0900

    aply 서비스 링크 목록 - 공통 스크립트 사용하게끔 개선

--- js/aqr_qr_creator.js
+++ js/aqr_qr_creator.js
@@ -210,20 +210,6 @@ const initQRCode = () => {
 		genQRCode(genStr);
 	});
 
-	$('#selSites').change(function () {
-		let targetVal = $(this).val();
-		let targetUrl = "";
-
-		targetsInfo.forEach(function (t) {
-			if (t[0] == targetVal) {
-				targetUrl = t[1];
-			}
-		});
-
-		if (targetUrl != "")
-			window.open(targetUrl, "_blank");
-	});
-
 	setSubmitHandler("email_up");
 	goToTop();
 
@@ -432,23 +418,3 @@ function showPrivacyDialog() {
     $('#modal_body_content').load("privacy_for_email.html");
     $('#modal-3').modal('show');
 }
-
-
-const targetsInfo = [
-	["aply", "https://aply.biz/"],
-	["aqr", "https://aqr.aplx.link"],
-	["recruit", "https://home.aply.biz/recruit/index.html"],
-	["duni", "https://duni.io"],
-	["dunipilot", "https://pilot.duni.io"],
-	["dunistock", "https://dunistock.com"],
-	["dromi", "https://dromi.aply.biz"],
-	["aplx", "https://home.aplx.link"],
-	["dkdk", "https://dkdk.io"],
-	["drdr", "https://drdr.io"],
-	["blog", "https://blog.naver.com/PostList.naver?blogId=jebo78&categoryNo=38&from=postList"],
-	["catchme", "https://catchme.aply.biz"],
-	["kiosk", "https://kiosk.aply.biz"],
-	["polaris", "https://polarisconsulting.modoo.at"],
-	["gps", "https://gps.aply.biz"],
-	["store", "https://smartstore.naver.com/aqr"]
-];
